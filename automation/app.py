a41df8ae44bb6fa897c4f28b6ece8d341c351a64
Kristian Todorov <63777932+kristian3551@users.noreply.github.com>
2022-12-07 20:25:47 +0200

    GUI refactoring

--- automation/app.py
+++ automation/app.py
@@ -5,6 +5,31 @@ from controller.controller import Controller
 
 # Control section
 
+class App:
+    def __init__(self) -> None:
+        self.root = Tk()
+        self.root.geometry(newGeometry="700x700")
+        self.controller: Controller = Controller()
+        self.controller.load_from_file()
+        self.current_auto_name: str = ""
+        self.message_label = ttk.Label(self.root)
+        self.message_label.grid(row=0)
+
+        self.control_section: ControlSection = ControlSection(self.root, self)
+        self.construction_section: ConstructionSection = ConstructionSection(self.root, self)
+        self.operation_section: OperationSection = OperationSection(self.root, self)
+
+    def set_message(self, message):
+        if self.message_label:
+            self.message_label.grid_forget()
+        self.message_label = ttk.Label(self.root, text=message)
+        self.message_label.grid()
+        self.message_label.grid(row=0)
+    def change_name(self, name):
+        self.current_auto_name = name
+    def run(self):
+        self.root.mainloop()
+
 class ControlSection:
     def __init__(self, frame, outer_section) -> None:
         self.outer_section = outer_section
@@ -36,11 +61,16 @@ class ControlSection:
     def load_image(self):
         if self.outer_section.operation_section.auto_image_label:
             self.outer_section.operation_section.auto_image_label.grid_forget()
+            self.outer_section.operation_section.name_label.grid_forget()
         auto_image = PhotoImage(file="automaton.gv.png")
         self.outer_section.operation_section.auto_image_label = ttk.Label(\
             self.outer_section.operation_section.section, image=auto_image)
         self.outer_section.operation_section.auto_image_label.image = auto_image
         self.outer_section.operation_section.auto_image_label.grid(row=1, column=1, rowspan=10)
+        self.outer_section.operation_section.name_label = ttk.Label(\
+            self.outer_section.operation_section.section,
+            text=f"Name: {self.name_entry.get()}")
+        self.outer_section.operation_section.name_label.grid(row=0, column=2)
         self.outer_section.root.update_idletasks()
     def load_auto(self, name):
         try:
@@ -84,24 +114,33 @@ class ControlSection:
 class ConstructionSection:
     def __init__(self, frame, outer_section) -> None:
         self.construction_section = ttk.Frame(frame)
+
         self.outer_section = outer_section
+
         self.determinize_button = ttk.Button(self.construction_section, text="Determinize",
         command=lambda: self.determinize(self.outer_section.control_section.name_entry.get()))
+
         self.minimize_button = ttk.Button(self.construction_section, text="Minimize",
         command=lambda: self.minimize(self.outer_section.control_section.name_entry.get()))
+
         self.concat_button = ttk.Button(self.construction_section, text="Concat",
         command=lambda: self.concat(self.outer_section.control_section.name_entry.get(),
          self.outer_section.control_section.regex_entry.get()))
+
         self.union_button = ttk.Button(self.construction_section, text="Union",
         command=lambda: self.union(self.outer_section.control_section.name_entry.get(),
          self.outer_section.control_section.regex_entry.get()))
+
         self.star_button = ttk.Button(self.construction_section, text="Kleene star",
         command=lambda: self.star(self.outer_section.control_section.name_entry.get()))
+
         self.total_button = ttk.Button(self.construction_section, text="Make total",
         command=lambda: self.total(self.outer_section.control_section.name_entry.get()))
+
         self.intersection_button = ttk.Button(self.construction_section, text="Intersection",
         command=lambda: self.intersect(self.outer_section.control_section.name_entry.get(),
          self.outer_section.control_section.regex_entry.get()))
+
         self.complement_button = ttk.Button(self.construction_section, text="Complement",
         command=lambda: self.complement(self.outer_section.control_section.name_entry.get()))
         self.render_elements()
@@ -128,7 +167,7 @@ class ConstructionSection:
 
     def determinize(self, name):
         try:
-            self.outer_section.controller.replace_or_add_automaton(name, 
+            self.outer_section.controller.replace_or_add_automaton(name,
                 self.outer_section.controller.determinize(name))
             self.outer_section.control_section.load_auto(name)
             self.outer_section.set_message("Successfully determinized automaton")
@@ -155,7 +194,7 @@ class ConstructionSection:
 
     def minimize(self, name):
         try:
-            self.outer_section.controller.replace_or_add_automaton(name, 
+            self.outer_section.controller.replace_or_add_automaton(name,
             self.outer_section.controller.minimize(name))
             self.outer_section.control_section.load_auto(name)
             self.outer_section.set_message("Successfully minimized automaton")
@@ -165,10 +204,12 @@ class ConstructionSection:
     def intersect(self, name1, name2):
         try:
             self.outer_section.controller.replace_or_add_automaton(name1,
-            self.outer_section.controller.concat(name1, name2))
+            self.outer_section.controller.intersection(name1, name2))
             self.outer_section.control_section.load_auto(name1)
             self.outer_section.set_message("Successfully concatenated automaton")
         except KeyError as error:
+            self.outer_section.set_message(error.args[0])
+        except ValueError as error:
             self.outer_section.set_message(error.args[0])
 
     def union(self, name1, name2):
@@ -197,13 +238,16 @@ class OperationSection:
         self.outer_section = outer_section
         self.auto_image_label = ttk.Label(self.section)
         self.auto_image_label.grid(column=1, row=2)
+        self.name_label = ttk.Label(self.section,
+         text="Name: ")
+        self.name_label.grid(column=2)
         self.input_section = InputSection(self.section, self)
         self.buttons_section = ButtonSection(self.section, self)
 
     # Input section
 
 class InputSection:
-    def __init__(self, frame, outer_section) -> None:
+    def __init__(self, frame, outer_section: OperationSection) -> None:
         self.outer_section = outer_section
         self.input_section = ttk.Frame(frame)
         self.accepts_word_entry = ttk.Entry(self.input_section, width=30)
@@ -219,7 +263,7 @@ class InputSection:
         self.transition_entry.insert(0, "Transitions: name letter name")
         self.transition_entry.grid()
         self.state_entry.grid()
-        self.input_section.grid(column=2)
+        self.input_section.grid(column=2, row=2)
 
     def accepts_word(self, word):
         try:
@@ -232,7 +276,7 @@ class InputSection:
 # Button section
 
 class ButtonSection:
-    def __init__(self, frame, outer_section) -> None:
+    def __init__(self, frame, outer_section: OperationSection) -> None:
         self.buttons_section = ttk.Frame(frame)
         self.outer_section = outer_section
         self.remove_state_button = ttk.Button(self.buttons_section, text="Remove state",
@@ -256,14 +300,14 @@ class ButtonSection:
         self.render()
 
     def render(self):
-        self.add_state_button.grid(row=2, column=1, padx=5)
-        self.remove_state_button.grid(row=2, column=2, padx=5)
-        self.add_start_button.grid(row=3, column=1, padx=5)
-        self.remove_start_button.grid(row=3, column=2, padx=5)
-        self.add_final_button.grid(row=4, column=1, padx=5)
-        self.remove_final_button.grid(row=4, column=2, padx=5)
-        self.add_transition_button.grid(row=5, column=1, padx=5)
-        self.remove_transition_button.grid(row=5, column=2)
+        self.add_state_button.grid(row=3, column=1, padx=5)
+        self.remove_state_button.grid(row=3, column=2, padx=5)
+        self.add_start_button.grid(row=4, column=1, padx=5)
+        self.remove_start_button.grid(row=4, column=2, padx=5)
+        self.add_final_button.grid(row=5, column=1, padx=5)
+        self.remove_final_button.grid(row=5, column=2, padx=5)
+        self.add_transition_button.grid(row=6, column=1, padx=5)
+        self.remove_transition_button.grid(row=6, column=2)
         self.buttons_section.grid(row=4, column=2, padx=5)
 
     def add_transition(self, raw):
@@ -363,31 +407,3 @@ class ButtonSection:
             control_section.load_auto(current_auto_name)
         except KeyError as error:
             self.outer_section.outer_section.set_message(error.args[0])
-
-# Functions
-
-class App:
-    def __init__(self):
-        self.root = Tk()
-        self.root.geometry(newGeometry="700x700")
-        self.controller = Controller()
-        self.controller.load_from_file()
-        self.current_auto_name = ""
-        self.message_label = ttk.Label(self.root)
-        self.message_label.grid(row=0)
-
-        self.control_section = ControlSection(self.root, self)
-        self.construction_section = ConstructionSection(self.root, self)
-        self.operation_section = OperationSection(self.root, self)
-
-    def set_message(self, message):
-        if self.message_label:
-            self.message_label.grid_forget()
-        self.message_label = ttk.Label(self.root, text=message)
-        self.message_label.grid()
-        self.message_label.grid(row=0)
-    def change_name(self, name):
-        self.current_auto_name = name
-    def run(self):
-        self.root.mainloop()
-
