9f4e6011a9f67ee83384b23f2c2005b771182774
Kristian Todorov <63777932+kristian3551@users.noreply.github.com>
2022-12-25 14:19:40 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-"""Entry point for the GUI/Console app."""
+"""Entry point for the GUI app."""
 
 from automation.app import App as GUIApp
-# from automation.engine import Engine as ConsoleApp
 
-app = GUIApp()
-app.run()
+if __name__ == "__main__":
+    app = GUIApp()
+    app.run()
